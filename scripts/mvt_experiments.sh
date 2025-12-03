5bae1b3267d6fbb59e931be22a579bd247a3d96c
EC2 Default User <ec2-user@ip-172-31-32-14.us-east-2.compute.internal>
2025-12-03 07:53:59 +0000

    vot manual testing

--- scripts/mvt_experiments.sh
+++ scripts/mvt_experiments.sh
@@ -11,15 +11,44 @@
 # # python run_training.py mvt DeT_MVT_Max
 # cd ../..
 
-# # cd DeT
-# # python pytracking/run_tracker.py mvt DeT_MVT_Max_KD001 --dataset_name depthtrack --input_dtype rgbcolormap --threads 4
-# # python pytracking/run_tracker.py mvt DeT_MVT_Max_KD002 --dataset_name depthtrack --input_dtype rgbcolormap --threads 4
-# # python pytracking/run_tracker.py mvt DeT_MVT_Max_CRD001 --dataset_name depthtrack --input_dtype rgbcolormap --threads 4
-# # python pytracking/run_tracker.py mvt DeT_MVT_Max_CRD002 --dataset_name depthtrack --input_dtype rgbcolormap --threads 4
-# # python pytracking/run_tracker.py mvt DeT_MVT_Max_KD001_CRD001 --dataset_name depthtrack --input_dtype rgbcolormap --threads 4
-# # python pytracking/run_tracker.py mvt DeT_MVT_Max_KD002_CRD002 --dataset_name depthtrack --input_dtype rgbcolormap --threads 4
+# cd DeT
+# python pytracking/run_tracker.py mvt DeT_MVT_Max_KD001 --dataset_name depthtrack --input_dtype rgbcolormap --threads 4
+# python pytracking/run_tracker.py mvt DeT_MVT_Max_KD002 --dataset_name depthtrack --input_dtype rgbcolormap --threads 4
+# python pytracking/run_tracker.py mvt DeT_MVT_Max_CRD001 --dataset_name depthtrack --input_dtype rgbcolormap --threads 4
+# python pytracking/run_tracker.py mvt DeT_MVT_Max_CRD002 --dataset_name depthtrack --input_dtype rgbcolormap --threads 4
+# python pytracking/run_tracker.py mvt DeT_MVT_Max_KD001_CRD001 --dataset_name depthtrack --input_dtype rgbcolormap --threads 4
+# python pytracking/run_tracker.py mvt DeT_MVT_Max_KD002_CRD002 --dataset_name depthtrack --input_dtype rgbcolormap --threads 4
 # # baselines
-# # python pytracking/run_tracker.py mvt DeT_MVT_Max --dataset_name depthtrack --input_dtype rgbcolormap --threads 4
+# python pytracking/run_tracker.py mvt DeT_DiMP50_Max_original_test --dataset_name depthtrack --input_dtype rgbcolormap --threads 4
+# python pytracking/run_tracker.py mvt DeT_DiMP50_Max --dataset_name depthtrack --input_dtype rgbcolormap --threads 4
+# python pytracking/run_tracker.py mvt DeT_MVT_Max --dataset_name depthtrack --input_dtype rgbcolormap --threads 4
+# cd ..
+
+cd DeT
+python pytracking/run_tracker.py mvt DeT_MVT_Max_KD001 --dataset_name votrgbd22 --input_dtype rgbcolormap --threads 4
+python pytracking/run_tracker.py mvt DeT_MVT_Max_KD002 --dataset_name votrgbd22 --input_dtype rgbcolormap --threads 4
+python pytracking/run_tracker.py mvt DeT_MVT_Max_CRD001 --dataset_name votrgbd22 --input_dtype rgbcolormap --threads 4
+python pytracking/run_tracker.py mvt DeT_MVT_Max_CRD002 --dataset_name votrgbd22 --input_dtype rgbcolormap --threads 4
+python pytracking/run_tracker.py mvt DeT_MVT_Max_KD001_CRD001 --dataset_name votrgbd22 --input_dtype rgbcolormap --threads 4
+python pytracking/run_tracker.py mvt DeT_MVT_Max_KD002_CRD002 --dataset_name votrgbd22 --input_dtype rgbcolormap --threads 4
+# baselines
+python pytracking/run_tracker.py mvt DeT_DiMP50_Max_original_test --dataset_name votrgbd22 --input_dtype rgbcolormap --threads 4
+python pytracking/run_tracker.py mvt DeT_DiMP50_Max --dataset_name votrgbd22 --input_dtype rgbcolormap --threads 4
+python pytracking/run_tracker.py mvt DeT_MVT_Max --dataset_name votrgbd22 --input_dtype rgbcolormap --threads 4
+cd ..
+
+
+# cd DeT
+# python pytracking/depthtrack_results.py --pred_dir pytracking/tracking_results/mvt/DeT_MVT_Max_KD001 --sequences_dir ../../data/depthtrack/test/
+# python pytracking/depthtrack_results.py --pred_dir pytracking/tracking_results/mvt/DeT_MVT_Max_KD002 --sequences_dir ../../data/depthtrack/test/
+# python pytracking/depthtrack_results.py --pred_dir pytracking/tracking_results/mvt/DeT_MVT_Max_CRD001 --sequences_dir ../../data/depthtrack/test/
+# python pytracking/depthtrack_results.py --pred_dir pytracking/tracking_results/mvt/DeT_MVT_Max_CRD002 --sequences_dir ../../data/depthtrack/test/  
+# python pytracking/depthtrack_results.py --pred_dir pytracking/tracking_results/mvt/DeT_MVT_Max_KD001_CRD001 --sequences_dir ../../data/depthtrack/test/
+# python pytracking/depthtrack_results.py --pred_dir pytracking/tracking_results/mvt/DeT_MVT_Max_KD002_CRD002 --sequences_dir ../../data/depthtrack/test/
+# # baselines
+# python pytracking/depthtrack_results.py --pred_dir pytracking/tracking_results/dimp/DeT_DiMP50_Max_original_test --sequences_dir ../../data/depthtrack/test/
+# python pytracking/depthtrack_results.py --pred_dir pytracking/tracking_results/dimp/DeT_DiMP50_Max --sequences_dir ../../data/depthtrack/test/
+# python pytracking/depthtrack_results.py --pred_dir pytracking/tracking_results/mvt/DeT_MVT_Max --sequences_dir ../../data/depthtrack/test/
 # cd ..
 
 cd DeT
